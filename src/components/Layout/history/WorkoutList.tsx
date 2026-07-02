296b34139b816cd338ab8ddda758572266021031
koraxd <bergariusz@gmail.com>
2026-07-02 14:10:37 +0100

    refactor: added new empty state message for filtering

--- src/components/Layout/history/WorkoutList.tsx
+++ src/components/Layout/history/WorkoutList.tsx
@@ -7,6 +7,7 @@ import { formatDate } from "@/utils/formatDate";
 import { useState } from "react";
 import { type Exercises } from "../log/WorkoutForm";
 import Button from "@/components/UI/Button/Button";
+import { Dumbbell, History } from "lucide-react";
 
 type WorkoutListProps = {
     initialExercises: Exercises
@@ -39,19 +40,43 @@ export default function WorkoutList({ initialExercises, workouts }: WorkoutListP
                     onChange={(e) => setDateFilter(e.target.value)}
                 />
             </div>
-            {filteredWorkouts.length === 0 && (
-                <div className="flex flex-col items-center gap-4 text-center py-20">
+            {workouts.length === 0 && (
+                <div className="flex flex-col items-center gap-4 text-center bg-card border border-subtle rounded-lg py-30 px-20">
+                    <Dumbbell size={30} className="rotate-y-180 text-text-muted" />
                     <div className="flex flex-col gap-2">
                         <h3 className="text-text-primary font-semibold text-xl">No workouts yet</h3>
-                        <p>Start logging your workouts to see them here</p>
+                        <p className="font-medium">Start logging your workouts to see them here</p>
                     </div>
-                    <Button variant="primary" size="sm" href="/dashboard/log">
+                    <Button 
+                        variant="primary" 
+                        size="sm" 
+                        href="/dashboard/log"
+                    >
                         Log Your First Workout
                     </Button>
                 </div>
             )}
+            {(filteredWorkouts.length === 0 && workouts.length !== 0) && (
+                <div className="flex flex-col items-center text-center gap-4 bg-card border border-subtle rounded-lg py-30 px-20">
+                    <History size={30} className="text-text-muted" />
+                    <div className="flex flex-col gap-2">
+                        <h3 className="text-text-primary font-semibold text-xl">No workouts found</h3>
+                        <p className="font-medium">Try adjusting your search or filters to find what you're looking for.</p>
+                    </div>
+                    <Button 
+                        variant="primary" 
+                        size="sm"
+                        onClick={() => {
+                            setSearchText("")
+                            setDateFilter("")
+                        }}
+                    >
+                        Clear Filters
+                    </Button>
+                </div>
+            )}
             <div className="flex flex-col gap-4">
-                {filteredWorkouts.map(workout => (
+                {workouts.length > 0 && filteredWorkouts.map(workout => (
                     <WorkoutCard 
                         key={workout.id} 
                         initialExercises={initialExercises}
