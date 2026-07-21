53edeb3baae538fac98792b9e011d604ca265a00
koraxd <bergariusz@gmail.com>
2026-07-21 11:15:33 +0100

    feat: added loading state when fetching chart data

--- src/components/Layout/progress/ProgressClient.tsx
+++ src/components/Layout/progress/ProgressClient.tsx
@@ -2,11 +2,13 @@
 
 import ProgressChart from "@/components/charts/ProgressChart"
 import ProgressDropdown from "./ProgressDropdown"
-import { Award } from "lucide-react"
+import { Award, Dumbbell } from "lucide-react"
 import { ExerciseTemplate } from "@/generated/prisma/client"
 import { useEffect, useState } from "react"
 import { getProgressData } from "@/actions/progress"
 import { formatDate } from "@/utils/formatDate"
+import Button from "@/components/UI/Button/Button"
+import Spinner from "@/components/UI/Spinner"
 
 type ProgressClientProps = {
     exercises: ExerciseTemplate[]
@@ -19,21 +21,25 @@ export default function ProgressClient({ exercises }: ProgressClientProps) {
         shortDate: string, 
         fullDate: Date 
     }[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
     useEffect(() => {
         if (!exerciseName) {
             return
         }
         
+        setIsLoading(true)
+
         const fetchData = async () => {
             const result = await getProgressData(exerciseName)
             setData(result)
+            setIsLoading(false)
         }
 
         fetchData()
     }, [exerciseName])
 
-    const pr = Math.max(...data.map(d => d.weight))
+    const pr = data.length > 1 ? Math.max(...data.map(d => d.weight)) : null
     const prDate = data.find(d => d.weight === pr)?.fullDate
     const formattedDate = prDate ? formatDate(prDate) : ""
 
@@ -50,16 +56,37 @@ export default function ProgressClient({ exercises }: ProgressClientProps) {
                         <h2 className="text-2xl font-semibold">Weight Progress</h2>
                         <p className="font-medium">Track your strength gains over time</p>
                     </div>
-                    <div className="bg-linear-to-br from-[#22C55E]/10 to-card border border-[#22C55E]/20 px-6 py-4 rounded-xl flex items-center gap-3">
-                        <Award className="text-white bg-[#22C55E] rounded-xl h-12 w-12 p-3" />
-                        <div className="flex flex-col">
-                            <span className="text-text-muted text-sm font-medium">Personal Record</span>
-                            <span className="text-text-primary text-xl font-semibold">{pr} kg</span>
-                            <span className="text-text-muted text-xs font-medium">{formattedDate}</span>
+                    {pr && (
+                        <div className="bg-linear-to-br from-[#22C55E]/10 to-card border border-[#22C55E]/20 px-6 py-4 rounded-xl flex items-center gap-3">
+                            <Award className="text-white bg-[#22C55E] rounded-xl h-12 w-12 p-3" />
+                            <div className="flex flex-col">
+                                <span className="text-text-muted text-sm font-medium">Personal Record</span>
+                                <span className="text-text-primary text-xl font-semibold">{pr} kg</span>
+                                <span className="text-text-muted text-xs font-medium">{formattedDate}</span>
+                            </div>
                         </div>
-                    </div>
+                    )}
                 </div>
-                <ProgressChart data={data} />
+                {isLoading ? (
+                    <div className="flex flex-col items-center gap-4">
+                        <Spinner className="w-10 h-10 border-8 border-brand border-t-brand-dark/40" />
+                    </div>
+                ) : data.length < 2 ? (
+                    <div className="flex flex-col items-center gap-4 text-center rounded-lg py-30 px-20">
+                        <Dumbbell size={30} className="rotate-y-180 text-text-muted" />
+                        <div className="flex flex-col gap-2">
+                            <h3 className="text-text-primary font-semibold text-xl">Not enough data</h3>
+                            <p className="font-medium">Log at least 2 sessions to see progress</p>
+                        </div>
+                        <Button 
+                            variant="primary" 
+                            size="sm" 
+                            href="/dashboard/log"
+                        >
+                            Log A Workout
+                        </Button>
+                    </div>
+                ) : <ProgressChart data={data} />}
             </div>
         </div>
     )
--- src/components/UI/Spinner.tsx
+++ src/components/UI/Spinner.tsx
@@ -1,5 +1,16 @@
-export default function Spinner() {
+import clsx from "clsx"
+
+type SpinnerProps = {
+    className?: string
+}
+
+export default function Spinner({ className }: SpinnerProps) {
     return (
-        <div className="w-5 h-5 border-4 border-text-primary border-t-brand rounded-full animate-spin"></div>
+        <div 
+            className={clsx(
+                "w-5 h-5 border-4 rounded-full animate-spin",
+                className
+            )} 
+        />
     )
 }
